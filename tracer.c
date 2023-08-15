808f8569114e0175a3be453261f657c86bca67af
maksym <vasyl.lanko@tecnico.ulisboa.pt>
2023-08-15 20:58:02 +0100

    code formatting

--- tracer.c
+++ tracer.c
@@ -8,8 +8,8 @@
 #include <unistd.h>
 #include <errno.h>
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
+int main(int argc, char* argv[]){
+    if (argc < 2){
         printf("Usage: %s <program> [args...]\n", argv[0]);
         return 1;
     }
@@ -20,31 +20,31 @@ int main(int argc, char* argv[]) {
 
     child = fork();
 
-    if (child == 0) {
+    if (child == 0){
         if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) < 0) {
             perror("ptrace");
             return 1;
         }
         execvp(argv[1], &argv[1]);
-    } else if (child > 0) {
+    } else if (child > 0){
         wait(&status);
         ptrace(PTRACE_SETOPTIONS, child, NULL, PTRACE_SETOPTIONS | PTRACE_O_TRACEFORK | PTRACE_O_TRACESYSGOOD | PTRACE_O_TRACECLONE);
 
-        while (WIFSTOPPED(status)) {
+        while (WIFSTOPPED(status)){
             ptrace(PTRACE_GETREGS, child, NULL, &regs);
 
             if (regs.orig_rax != -1){
                 printf("FROM: %d, Syscall %ld: rdi=%ld, rsi=%ld, rdx=%ld, r10=%ld\n",
                         child, regs.orig_rax, regs.rbx, regs.rcx, regs.rdx, regs.r10);
             }
-            if (ptrace(PTRACE_SYSCALL, child, NULL, NULL) < 0) {
+            if (ptrace(PTRACE_SYSCALL, child, NULL, NULL) < 0){
                 perror("ptrace");
                 return 1;
             }
 
             wait(&status);
         }
-    } else {
+    } else{
         perror("fork");
         return 1;
     }
